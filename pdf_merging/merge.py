93b922074f6fc561aeeb082447943edcde3637fe
serbansandu7 <serban.sandu@bytex.ro>
2019-06-18 17:31:53 +0300

    Added code for testing

--- pdf_merging/merge.py
+++ pdf_merging/merge.py
@@ -7,6 +7,28 @@ of the pdfs in the <pdfs> folder in the <merge> folder
 hint: use mstamy2's PyPDF2
 """
 
-import PyPDF2
 
 DESTINATION_MERGE = "merge/merge.pdf"
+
+import os
+import sys
+
+from PyPDF2 import PdfFileMerger
+
+
+folder = "pdfs"
+
+documente = os.listdir(folder)
+
+merger = PdfFileMerger()
+
+for doc in documente:
+    if not doc.endswith(".pdf"):
+        continue
+
+    input1 = open(os.path.join(folder, doc), 'rb')
+    print(os.path.join(folder, doc))
+    merger.append(input1)
+
+output = open(DESTINATION_MERGE, "wb")
+merger.write(output)
