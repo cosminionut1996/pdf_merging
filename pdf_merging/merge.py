af167b18e7a96665ab27c42a84ea1c01756381de
Ionut Cosmin <ionutcosmin@ICS-MBP.local>
2019-06-18 17:11:29 +0300

    updated yaml

--- pdf_merging/merge.py
+++ pdf_merging/merge.py
@@ -1,7 +1,12 @@
 """
 Write your code here
 
+Running this script should write a concatenation
+of the pdfs in the <pdfs> folder in the <merge> folder
 
+hint: use mstamy2's PyPDF2
 """
 
-print("Mycode")
+import PyPDF2
+
+DESTINATION_MERGE = "merge/merge.pdf"
