87196bf9c55249e32c1613dfb2ef8eddd1990e1f
Ionut Cosmin <ionutcosmin@ICS-MBP.local>
2019-06-18 17:45:10 +0300

    testing the number of pages

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,5 +1,6 @@
 import hashlib
 import unittest
+from PyPDF2 import PdfFileReader
 
 def md5(fname):
     hash_md5 = hashlib.md5()
@@ -12,10 +13,10 @@ class TestMerge(unittest.TestCase):
 
     def test_result(self):
         merge_result_location = 'merge/merge.pdf'
-        self.assertEqual(
-            "e661d4ff8a10c1c2efacaae3bd217837",
-            md5(merge_result_location)
-        )
+        pdf = PdfFileReader(open(merge_result_location,'rb'))
+        pages = pdf.getNumPages()
+
+        self.assertEqual(pages, 66)
 
 if __name__ == "__main__":
     unittest.main()
