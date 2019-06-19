27315cf25e52432f22a5308d4f32173715e018c7
Bytex <bytex@Dragoss-MacBook-Air.local>
2019-06-19 10:12:26 +0200

    msg

--- pdf_merging/merge.py
+++ pdf_merging/merge.py
@@ -9,5 +9,31 @@ hint: use mstamy2's PyPDF2
 
 import PyPDF2
 
+from sys import stderr, stdout, exit
+import os
+import traceback
+from collections import defaultdict
+
+from PyPDF2 import PdfFileMerger, parse_filename_page_ranges
+
 DESTINATION_MERGE = "merge/merge.pdf"
 PDFS_FOLDER = "pdfs"
+
+
+if __name__ == "__main__":
+	merger = PdfFileMerger()
+	filePath = PDFS_FOLDER
+	
+	filename_page_ranges = parse_filename_page_ranges(os.listdir(PDFS_FOLDER))
+	for (filename, page_range) in filename_page_ranges:
+		#print(filename, page_range)
+		fp = open(filePath + '/' + filename, "rb")
+		merger.append(fp, pages=page_range)
+		fp.close()
+
+	output = open(DESTINATION_MERGE, 'wb')
+	merger.write(output)
+	output.close()
+		
+
+
