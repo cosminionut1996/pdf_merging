2662a7cf08753a5447fd9d892f3b18e93bf2c9e5
Ionut Cosmin <ionutcosmin@ICS-MBP.local>
2019-06-26 11:48:03 +0300

    modified

--- pdf_merging/merge.py
+++ pdf_merging/merge.py
@@ -7,7 +7,26 @@ of the pdfs in the <pdfs> folder in the <merge> folder
 hint: use mstamy2's PyPDF2
 """
 
-import PyPDF2
+from PyPDF2 import PdfFileMerger
+import os.path 
+
 
 DESTINATION_MERGE = "merge/merge.pdf"
 PDFS_FOLDER = "pdfs"
+list_test = ['1.pdf','2.pdf','3.pdf','4.pdf']
+
+
+def merge_pdf():
+    mergePdf = PdfFileMerger()
+    for i in range(4):
+        
+        s = os.path.join(PDFS_FOLDER,list_test[i])
+    
+        with open(os.path.join(PDFS_FOLDER,list_test[i]),'rb') as f:
+            mergePdf.append(f)
+    return mergePdf
+
+
+merge_pdf_writer = merge_pdf()
+output = open(DESTINATION_MERGE,"wb")
+merge_pdf_writer.write(output)
